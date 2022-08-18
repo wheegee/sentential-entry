659a3fa35c6dc7f2e90a0604c6e73d8f6d175301
Raymond Douglas <r@rymnd.org>
2022-08-17 19:50:08 -0700

    initial `env` functionality

---
+++ README.md
@@ -0,0 +1,19 @@
+# sentential-entry
+
+[![crates.io](https://img.shields.io/crates/v/sentential.svg)](https://crates.io/crates/sentential)
+
+sentential-entry is an execution wrapper for [sententially-driven](https://github.com/wheegee/sentential) Lambdas on AWS.
+
+## Install
+
+With crates.io:
+```bash
+cargo install sentential
+```
+
+From source:
+```bash
+cargo install --path /path/to/sentential-entry/repo
+```
+
+Or, download and run the binary from [the latest release](https://github.com/wheegee/sentential-entry/releases).
---
+++ src/main.rs
@@ -0,0 +1,105 @@
+use aws_config::meta::region::RegionProviderChain;
+use aws_sdk_ssm::{Client, Region};
+use clap::Parser;
+use std::collections::HashMap;
+use std::error::Error;
+use std::process::Command;
+
+/// Wrapper for sententially-driven execution
+#[derive(Parser)]
+#[clap(version, about, long_about = None)]
+struct Options {
+    /// Verbose mode
+    #[clap(short, long)]
+    verbose: bool,
+
+    /// AWS region to use
+    #[clap(short, long, env = "AWS_REGION")]
+    region: Option<String>,
+
+    #[clap(subcommand)]
+    sub: Sub,
+}
+
+#[derive(clap::Subcommand)]
+enum Sub {
+    Env(Env),
+}
+
+/// SSM -> Environment -> Command
+#[derive(Parser)]
+struct Env {
+    /// SSM prefix to filter
+    prefix: String,
+
+    /// Command to execute
+    command: Vec<String>,
+}
+
+/// Fetch parameters in given SSM prefix.
+///
+/// Return parameters as `HashMap`.
+async fn fetch_parameters(
+    client: &Client,
+    prefix: &str,
+) -> Result<HashMap<String, String>, Box<dyn Error>> {
+    let mut parameters = HashMap::new();
+
+    let resp = client
+        .get_parameters_by_path()
+        .path(prefix)
+        .with_decryption(true)
+        .send()
+        .await?;
+
+    for parameter in resp.parameters.unwrap().iter() {
+        parameters.insert(
+            parameter.name().unwrap().to_string().replace(prefix, ""),
+            parameter.value().unwrap().to_string(),
+        );
+    }
+
+    Ok(parameters)
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let Options {
+        verbose,
+        region,
+        sub,
+    } = Options::parse();
+
+    // AWS configuration
+    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
+        .or_default_provider()
+        .or_else(Region::new("us-west-2"));
+    let shared_config = aws_config::from_env().region(region_provider).load().await;
+    let client = Client::new(&shared_config);
+
+    // Verbose output
+    if verbose {
+        println!("Region: {}", shared_config.region().unwrap());
+    }
+
+    // Subcommand execution
+    match sub {
+        Sub::Env(c) => {
+            let parameters = match fetch_parameters(&client, &c.prefix).await {
+                Ok(parameters) => parameters,
+                Err(error) => panic!(
+                    "error: failed to retrieve parameters under `{}`: {:?}",
+                    &c.prefix, error
+                ),
+            };
+
+            Command::new(&c.command[0])
+                .args(&c.command[1..])
+                .envs(&parameters)
+                .spawn()
+                .expect("error: failed to execute given command");
+        }
+    }
+
+    Ok(())
+}
